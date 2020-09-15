ac75c6d25163f2e4cd3ed4404db25ee4e5151f7a
tamalbasu <tamal.basu26@gmail.com>
2020-09-15 23:54:05 +0530

    springboot Setup

---
+++ src/main/java/com/tiny/firstJava.java
@@ -0,0 +1,10 @@
+package com.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class firstJava {
+    public static void main(String[] args) {
+        SpringApplication.run(firstJava.class, args);
+    }}
